d8decd44fe7e0f7ed961c16045d5b13617386278
Nil Shah <nilshah.31@gmail.com>
2018-10-30 21:04:51 +0530

    Implements Print

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { CollectionView } from 'wijmo/wijmo';
 import { HotTableRegisterer } from '@handsontable/angular'
+import { print } from 'printing'
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dataset = [[]]
-  title = 'Wijmo Starter App';
   hotId: string = 'tempID';
   tableSettings: {};
+  hotInstance: any
+  printInstance: any
+  dataset = [[]]
+  title = 'Wijmo Starter App';
   data = this.getData();
-  hotInstance : any
-  constructor( private hotRegisterer: HotTableRegisterer){
-    
+  constructor(private hotRegisterer: HotTableRegisterer) {
+    this.printInstance = print;
   }
 
   ngOnInit() {
@@ -34,7 +37,7 @@ export class AppComponent {
     return new CollectionView(data);
   }
 
-  iconClickHandle (item) {
+  iconClickHandle(item) {
     console.log(this.dataset);
   }
 
@@ -51,6 +54,23 @@ export class AppComponent {
     this.hotInstance.undo()
   }
   printPage() {
-    window.print();    
+    // setTimeout($.unblockUI, 2000);
+    const options = {
+      // modify DOM before print
+      beforePrint(window) { },
+      // paper direction, default is 'vertical'
+      direction: 'vertical',
+      // scan and apply specified styles, default is true (all styles)
+      scanStyles: true,
+      // inject css, default is ''
+      css: '',
+      // inject all links and styles from source page, default is false
+      injectGlobalCss: false,
+    }
+    const target = document.getElementById(this.hotId)
+    // print
+    this.printInstance(target, options).then(() => {
+      // after print
+    })
   }
 }
