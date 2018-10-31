360374086a5473c1827cbe2524534fdf96bfffbb
Nil Shah <nilshah.31@gmail.com>
2018-10-31 21:21:51 +0530

    Implements print

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CollectionView } from 'wijmo/wijmo';
-import { HotTableRegisterer } from '@handsontable/angular'
+import { HotTableRegisterer,HotTableModule } from '@handsontable/angular'
 import { print } from 'printing'
 declare var $: any;
 @Component({
@@ -10,19 +10,39 @@ declare var $: any;
 })
 export class AppComponent {
   hotId: string = 'tempID';
+  printHotId:string = "printTempID"
+  numberOfRows:number = 1000
+  numberOfCols:number = 26;
   tableSettings: {};
   hotInstance: any
   printInstance: any
   dataset = [[]]
+  printDataSet = [[]]
   title = 'Wijmo Starter App';
   data = this.getData();
+  displayPrintTable:boolean=false;
+  printCols:number = 0
+  printRows:number = 0
   constructor(private hotRegisterer: HotTableRegisterer) {
     this.printInstance = print;
   }
 
   ngOnInit() {
-  }
+    
 
+  }
+  ngAfterViewInit(){
+    this.hotInstance = this.hotRegisterer.getInstance(this.hotId);
+    this.hotInstance.addHook('afterChange', this.afterChangeSheetCallback)
+  }
+  afterChangeSheetCallback(changes) {
+    let rowRange = parseInt($("#rowRange").val());
+    let colRange = parseInt($("#colRange").val());
+    changes.forEach(([row, prop, oldValue, newValue]) => {
+      if(prop>colRange) $("#colRange").attr('value', prop);
+      if(row>rowRange) $("#rowRange").val(row.toString())
+    });
+  }
   getData() {
     var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(','),
       data = [];
@@ -48,29 +68,45 @@ export class AppComponent {
   redoPage() {
     this.hotInstance = this.hotRegisterer.getInstance(this.hotId);
     this.hotInstance.redo()
+
   }
   undoPage() {
     this.hotInstance = this.hotRegisterer.getInstance(this.hotId);
     this.hotInstance.undo()
   }
   printPage() {
-    // setTimeout($.unblockUI, 2000);
+    this.printDataSet = this.hotInstance.getData()
+    this.printRows = parseInt($("#rowRange").val());
+    this.printCols = parseInt($("#colRange").val());
+    let data       = this.hotInstance.getData()
+    // data = data.splice(0,rowRange+1)
+    // for(let i=0;i<=rowRange;i++){
+    //     data[i] = data[i].splice(0,colRange+1)
+    // }
+    this.printDataSet = data;
     const options = {
-      // modify DOM before print
-      beforePrint(window) { },
-      // paper direction, default is 'vertical'
-      direction: 'vertical',
-      // scan and apply specified styles, default is true (all styles)
-      scanStyles: true,
-      // inject css, default is ''
-      css: '',
-      // inject all links and styles from source page, default is false
-      injectGlobalCss: false,
-    }
-    const target = document.getElementById(this.hotId)
-    // print
-    this.printInstance(target, options).then(() => {
-      // after print
+        // modify DOM before print
+        beforePrint(window) { },
+        // paper direction, default is 'vertical'
+        direction: 'vertical',
+        // scan and apply specified styles, default is true (all styles)
+        scanStyles: true,
+        // inject css, default is ''
+        css: '',
+        // inject all links and styles from source page, default is false
+        injectGlobalCss: false,
+      }
+    this.displayPrintTable = true;
+    let self = this;
+    setTimeout(function(){self.printSheet(options)},1000)
+     
+  }
+  printSheet(options){
+    let self = this;
+    const target = document.getElementById(this.printHotId)
+    this.printInstance(target, options).then(()=>{
+      this.displayPrintTable = false;
     })
+    
   }
 }
